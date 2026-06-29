37ff19e57a04cef37387fbaedec816b61e3e484e
Diego Alegre <111524804+EzeAlegre@users.noreply.github.com>
2026-06-29 16:20:03 -0300

    Add files via upload

---
+++ config/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+admin.site.site_header = 'Comunidades Unidas - Administración'
+admin.site.site_title = 'Comunidades Unidas'
+admin.site.index_title = 'Panel de administración'
